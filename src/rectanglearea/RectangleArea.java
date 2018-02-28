e11faf0d263c5fabe8821b5efdffde1dd5789024
xagod6351 <xagod6351@7KXN6Y1.Student.UGDSB.ED>
2018-02-28 13:57:06 -0500

    First time ;)

---
+++ src/rectanglearea/RectangleArea.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rectanglearea;
+
+/**
+ *
+ * @author xagod6351
+ */
+public class RectangleArea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    double length, width, area;
+    length=5.7;
+    width=4.8;
+    area=length*width;
+    System.out.println(area);
+    }
+    }
+    
+
